1c7ccb844aaa36fc55bccb927d04a9cd5f50b41a
Vaclav Pavlin <vpavlin@redhat.com>
2015-03-25 15:50:21 -0400

    Add support to run meta-image

--- app-ent.py
+++ app-ent.py
@@ -9,11 +9,15 @@ import ConfigParser
 from string import Template
 import tempfile
 import subprocess
+import anymarkup
+import docker
+import distutils.dir_util
 
 ATOMIC_FILE="Atomicfile"
-PARAMS_FILE="params.ini"
-ANSWERS_FILE="answers.ini"
-K8S_DIR="Kubernetes"
+PARAMS_FILE="params.conf"
+ANSWERS_FILE="answers.conf"
+K8S_DIR="graph"
+APP_ENT_PATH="application-entity"
 
 
 class AppEnt():
@@ -23,11 +27,17 @@ class AppEnt():
     params_data = None
     answers_data = None
     tmpdir = None
-    def __init__(self, answers, dryrun = False, debug = False):
+    def __init__(self, answers, app, dryrun = False, debug = False):
         self.debug = debug
         self.dryrun = dryrun
 
         self.tmpdir = tempfile.mkdtemp(prefix="appent-")
+        if self.debug:
+            print(self.tmpdir)
+
+        if not os.path.exists(app):
+            self._pullApp(app)
+            self._populateMainApp()
 
         if not self._loadAtomicfile(os.path.join(os.getcwd(), ATOMIC_FILE)):
             print("Failed to load %s" % ATOMIC_FILE)
@@ -41,7 +51,7 @@ class AppEnt():
             return
         
         if self.debug:
-            print(self.params_data.sections())
+            print(self.params_data)
 
         if not self._loadAnswers(answers):
             print("No %s file found, using defaults" % ANSWERS_FILE)
@@ -56,17 +66,17 @@ class AppEnt():
         if not os.path.exists(path):
             return None
 
-        with open(path, "r") as fp:
-            self.atomicfile_data = json.load(fp)
+        self.atomicfile_data = anymarkup.parse_file(path)
 
         return self.atomicfile_data
 
     def _loadParams(self, path = None):
         if not os.path.exists(path):
             return None
-
-        self.params_data = ConfigParser.ConfigParser()
-        self.params_data.read([path])
+        if self.params_data:
+            self.params_data.update(anymarkup.parse_file(path))
+        else:
+            self.params_data = anymarkup.parse_file(path)
 
         return self.params_data
 
@@ -74,8 +84,7 @@ class AppEnt():
         if not os.path.exists(path):
             return None
 
-        self.answers_data = ConfigParser.ConfigParser()
-        self.answers_data.read([path])
+        self.answers_data = anymarkup.parse_file(path)
 
         return self.answers_data
 
@@ -90,20 +99,26 @@ class AppEnt():
 
         for component in self.atomicfile_data["graph"]:
             component_path = self._getComponentDir(component)
-            if os.path.isdir(component_path):
-                self._processComponent(component)
-            else:
-                raise Exception("Data for component %s is missing" % component)
+            if not os.path.isdir(component_path):
+                self._pullApp(component)
+
+            component_params = os.path.join(component_path, PARAMS_FILE)
+            if os.path.isfile(component_params):
+                self._loadParams(component_params)
+            self._processComponent(component)
 
     def _applyTemplate(self, data, component):
         template = Template(data)
-        if not self.params_data.has_section(component):
+
+        d = {}
+        if "general" in self.params_data:
+            d = self.params_data["general"]
+        if component in self.params_data:
+            d.update(self.params_data[component])
+        else:
             print("No defaults for %s" % component)
-            return data
-        d = dict(self.params_data.items("general"))
-        d.update(dict(self.params_data.items(component)))
-        if self.answers_data and self.answers_data.has_section(component):
-            d.update(dict(self.answers_data.items(component)))
+        if self.answers_data and component in self.answers_data:
+            d.update(self.answers_data[component])
 
         return template.substitute(d)
 
@@ -120,21 +135,55 @@ class AppEnt():
         return False
 
     def _processComponent(self, component):
-        kube_artifacts = ["service", "rc", "pod"] #FIXME
-        for artifact in kube_artifacts:
+        kube_order = ["service", "rc", "pod"] #FIXME
+        kube_artifacts = {"service":{}, "rc":{}, "pod":{}}
+        path = os.path.join(self._getComponentDir(component))
+        for entity in os.listdir(path):
+            print(entity)
+            data = anymarkup.parse_file(os.path.join(path, entity))
+            if "kind" in data:
+                kube_artifacts[data["kind"].lower()] = data
+            else:
+                print("Malformed kube file")
+
+        for artifact in kube_order:
             filename = "%s-%s.json" % (component, artifact)
-            path = os.path.join(self._getComponentDir(component), filename)
-            if not os.path.exists(path):
-                continue
-            data = None
-            with open(path) as fp:
-                data = self._applyTemplate(fp.read(), component)
+            data = self._applyTemplate(anymarkup.serialize(kube_artifacts[artifact], "json"), component)
         
             k8s_file = os.path.join(self.tmpdir, filename)
             with open(k8s_file, "w") as fp:
                 fp.write(data)
 
             self._callK8s(k8s_file)
+
+    def _pullApp(self, app):
+        pull = ["docker", "pull", app]
+        subprocess.call(pull)
+        data_list = ["/Atomicfile",
+                "/params.conf",
+                "/graph/%s/pod.json" % app,
+                "/graph/%s/replication_controller.json" % app,
+                "/graph/%s/service.json" % app,
+                "/graph/%s/params.conf" % app
+                ]
+    
+        name = os.path.basename(app).split(":")[0]
+        create = ["docker", "create", "--name", name, app, "nop"]
+        subprocess.call(create)
+        cp = ["docker", "cp", "%s:/%s" % (name, APP_ENT_PATH), self.tmpdir]
+        subprocess.call(cp)
+
+        rm = ["docker", "rm", name]
+        subprocess.call(rm)
+
+
+#        client.remove_container(container, force=True)
+
+    def _populateMainApp(self):
+        print("Copying")
+        distutils.dir_util.copy_tree(os.path.join(self.tmpdir, APP_ENT_PATH), os.getcwd())
+
+
 
 
 
@@ -143,9 +192,10 @@ if __name__ == "__main__":
     parser.add_argument("-d", "--debug", dest="debug", default=False, action="store_true", help="Debug")
     parser.add_argument("--dry-run", dest="dryrun", default=False, action="store_true", help="Don't call k8s")
     parser.add_argument("-a", "--answers", dest="answers", default=os.path.join(os.getcwd(), ANSWERS_FILE), help="Path to %s" % ANSWERS_FILE)
+    parser.add_argument("app", help="App to run")
 
     args = parser.parse_args()
 
-    ae = AppEnt(args.answers, args.dryrun, args.debug)
+    ae = AppEnt(args.answers, args.app, args.dryrun, args.debug)
 
     sys.exit(0)
