cf6d45d48836eb7e2401b32668541d243b4a1203
Vaclav Pavlin <vpavlin@redhat.com>
2015-05-26 17:01:37 +0200

    Remove SCHEMA_URL as it's not used anymore after create.py removal

--- atomicapp/constants.py
+++ atomicapp/constants.py
@@ -15,6 +15,3 @@ WORKDIR=".workdir"
 
 DEFAULT_PROVIDER="kubernetes"
 DEFAULT_ANSWERS={"general":{"provider":DEFAULT_PROVIDER}}
-
-SCHEMA_URL="https://raw.githubusercontent.com/projectatomic/nulecule/master/spec/0.0.1-alpha/schema.json"
-
