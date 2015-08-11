9e4628c643fd46d96f25da87f977feb495d7897e
Václav Pavlín <vaclav.pavlin@gmail.com>
2015-08-11 15:04:17 +0200

    Use /run rather than /var

--- atomicapp/constants.py
+++ atomicapp/constants.py
@@ -11,7 +11,7 @@ ANSWERS_FILE = "answers.conf"
 ANSWERS_FILE_SAMPLE = "answers.conf.sample"
 ANSWERS_FILE_SAMPLE_FORMAT = 'ini'
 WORKDIR = ".workdir"
-LOCK_FILE = "/var/lock/atomicapp.lock"
+LOCK_FILE = "/run/lock/atomicapp.lock"
 
 DEFAULT_PROVIDER = "kubernetes"
 DEFAULT_NAMESPACE = "default"
