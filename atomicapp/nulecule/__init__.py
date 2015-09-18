7bae26cc98a518420fc6fcf32ec2e5e16299dd1c
Ratnadeep Debnath <rtnpro@gmail.com>
2015-09-18 23:35:18 +0530

    Added code to load config for a Nulecule app and it's internal components.

--- atomicapp/nulecule/__init__.py
+++ atomicapp/nulecule/__init__.py
@@ -1,10 +1,23 @@
-class NuleculeConfig(object):
-    pass
+# -*- coding: utf-8 -*-
+import copy
+from atomicapp.utils import Utils
 
 
 class NuleculeBase(object):
-
     def load(self):
+        pass
+
+    def load_config(self, config, params, namespace):
+        for param in params:
+            value = config.get(namespace).get(param['name']) or \
+                config.get('general').get(param['name']) or \
+                param.get('default')
+            if value is None:
+                value = Utils.askFor(param['name'], param)
+            config[namespace][param['name']] = value
+        return config
+
+    def load_params(self, params):
         pass
 
     def run(self, provider):
@@ -22,29 +35,40 @@ class NuleculeBase(object):
 
 class Nulecule(NuleculeBase):
     """
-    This represents a Nulecule application. A Nulecule instance can have
-    instances of Nulecule and Nulecule as components. A Nulecule instance
-    knows everything about itself and its componenents, but does not have
-    access to it's parent's scope.
+    This represents an application compliant with Nulecule specification.
+    A Nulecule instance can have instances of Nulecule and Nulecule as
+    components. A Nulecule instance knows everything about itself and its
+    componenents, but does not have access to it's parent's scope.
     """
     def __init__(self, id, specversion, metadata, graph, requirements=None,
-                 params=None, config=None):
+                 params=None, config=None, namespace='general'):
         self.id = id
         self.specversion = specversion
         self.metadata = metadata
         self.graph = graph
         self.requirements = requirements
-        self.params = params
+        self.params = params or []
+        self.namespace = namespace
+        self.config = self.load_config(config, self.params, self.namespace)
         self.load()
 
     def load(self):
-        self.components = self.load_components()
-        self.config = self.load_config()
+        self.components = self.load_components(self.graph)
 
     def load_components(self, graph):
         components = []
-        for item in graph:
-            components.append(NuleculeComponent(item))
+        for node in graph:
+            node_name = node['name']
+            if node.get('artifacts'):
+                config = {}
+                config['general'] = copy.deepcopy(self.config.get('general'))
+                config[node_name] = copy.deepcopy(self.config.get(node_name))
+                component = NuleculeComponent(
+                    node_name, node.get('source'), node.get('params'),
+                    node.get('artifacts'), config=config)
+                self.config[node_name].update(component.config.get(node_name))
+                components.append(component)
+        return components
 
     def run(self):
         # run the Nulecule application
@@ -65,10 +89,28 @@ class Nulecule(NuleculeBase):
 
 class NuleculeComponent(NuleculeBase):
     """
-    Represents a local service in a Nulecule application. It receives props
+    Represents a component in a Nulecule application. It receives props
     from it's parent and can add new props and override props at it's local
     scope. It does not have direct access to props of sibling Nulecule
     components, but can request the value of sibling's property from it's
     parent.
     """
-    pass
+    def __init__(self, name, source=None, params=None, artifacts=None, config=None):
+        self.name = name
+        self.source = source
+        self.params = params or []
+        self.artifacts = artifacts
+        self.config = self.load_config(config, self.params, name)
+        self.load()
+
+    def load(self):
+        if self.artifacts:
+            self.load_artifact()
+        else:
+            self.load_external_application()
+
+    def load_artifact(self):
+        pass
+
+    def load_external_application(self):
+        pass
