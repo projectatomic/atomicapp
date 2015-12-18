457486018883fd9336e9d692c5d7845c5fcdfa5b
Ratnadeep Debnath <rtnpro@gmail.com>
2015-12-18 17:19:53 +0530

    Added tests for openshift _process_artifact_data.

--- tests/units/providers/test_openshift_provider.py
+++ tests/units/providers/test_openshift_provider.py
@@ -2,6 +2,7 @@
 import unittest
 import mock
 from atomicapp.providers.openshift import OpenShiftProvider
+from atomicapp.plugin import ProviderFailedException
 
 
 class OpenshiftProviderTestMixin(object):
@@ -60,8 +61,81 @@ class TestOpenshiftProviderDeploy(OpenshiftProviderTestMixin, unittest.TestCase,
         self.assertFalse(self.mock_oc.deploy.call_count)
 
 
-class TestOpenshiftProviderProcessArtifacts(unittest.TestCase):
-    pass
+class TestOpenshiftProviderProcessArtifactData(OpenshiftProviderTestMixin, unittest.TestCase):
+
+    def test_process_artifact_data_non_template_kind(self):
+        artifact_data = {
+            'kind': 'Pod',
+            'pods': [
+                {
+                    'metadata': {
+                        'namespace': 'foo'
+                    }
+                }
+            ]
+        }
+        self.mock_oc.get_oapi_resources.return_value = ['pods']
+
+        op = self.get_oc_provider()
+
+        op._process_artifact_data('foo', artifact_data)
+
+        self.assertEqual(op.openshift_artifacts,
+                         {'pod': [artifact_data]})
+
+    def test_process_artifact_data_template_kind(self):
+        artifact_data = {
+            'kind': 'Template',
+            'objects': [
+                {
+                    'kind': 'Pod',
+                    'metadata': {
+                        'namespace': 'foo'
+                    }
+                },
+                {
+                    'kind': 'Service',
+                    'metadata': {
+                        'namespace': 'foo'
+                    }
+                }
+            ]
+        }
+        self.mock_oc.get_oapi_resources.return_value = ['templates']
+        op = self.get_oc_provider()
+        self.mock_oc.process_template.return_value = artifact_data['objects']
+
+        op._process_artifact_data('foo', artifact_data)
+
+        self.assertEqual(
+            op.openshift_artifacts, {
+                'pod': [
+                    {'kind': 'Pod', 'metadata': {'namespace': 'foo'}}
+                ],
+                'service': [
+                    {'kind': 'Service', 'metadata': {'namespace': 'foo'}}
+                ]
+            }
+        )
+
+    def test_process_artifact_data_error_resource_not_in_resources(self):
+        artifact_data = {
+            'kind': 'foobar'
+        }
+
+        op = self.get_oc_provider()
+
+        self.assertRaises(
+            ProviderFailedException,
+            op._process_artifact_data, 'foo', artifact_data)
+
+    def test_process_artifact_data_error_kind_key_missing(self):
+        artifact_data = {}
+        op = self.get_oc_provider()
+
+        self.assertRaises(
+            ProviderFailedException,
+            op._process_artifact_data, 'foo', artifact_data)
 
 
 class TestOpenshiftProviderProcessTemplate(unittest.TestCase):
