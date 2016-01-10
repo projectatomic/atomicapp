b64068d468cf5b7141c2fb856868a39bc70b51af
Brian (bex) Exelbierd <bex@pobox.com>
2016-01-10 20:57:53 +0100

    Fixing the build_run.sh Dockerfile path
    This allows you to build both .git and .pkgs containers

--- build_run.sh
+++ build_run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 WHAT=$1
+DISTRO=$2
 
 # TODO sanity check that we got docker >= 1.6
 
-[ -z "${WHAT}" ] && echo "Need to provide a distro you want to build for (fedora|centos|rhel7|debian)" && exit
-IMAGE_NAME=atomicapp-${WHAT}
+[ -z "${WHAT}" ] && echo "Need to provide a source location to build from (git|pkgs)" && echo "usage: build_run.sh SRC_LOCATION DISTRO" && exit
+
+[ -z "${DISTRO}" ] && echo "Need to provide a distro you want to build for (fedora|centos|rhel7|debian)" && echo "usage: build_run.sh SRC_LOCATION DISTRO" && exit
+
+IMAGE_NAME=atomicapp-${WHAT}-${DISTRO}
 
 if [ -z "$USERNAME" ]; then
     echo "setting USERNAME to " `whoami` 
@@ -13,7 +17,7 @@ if [ -z "$USERNAME" ]; then
 fi
 
 echo docker build $USERNAME/$IMAGE_NAME
-docker build --rm --tag $USERNAME/$IMAGE_NAME --file Dockerfile.${WHAT} .
+docker build --rm --tag $USERNAME/$IMAGE_NAME --file Dockerfiles.${WHAT}/Dockerfile.${DISTRO} .
 
 #doesn't really make sense to run it
 #test
