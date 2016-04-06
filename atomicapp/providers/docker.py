7c9cad933cd0588f3ba8395901d1ed9d4b1145d3
Shubham Minglani <shubham@linux.com>
2016-04-06 21:58:22 +0530

    replace inspect with ps, fix #672

--- atomicapp/providers/docker.py
+++ atomicapp/providers/docker.py
@@ -72,7 +72,7 @@ class DockerProvider(Provider):
                 raise ProviderFailedException(msg)
 
     def _get_containers(self):
-        docker_cmd = 'docker inspect --format="{{ .Name }}" $(docker ps -aq --no-trunc) | sed "s,/,,g"'
+        docker_cmd = 'docker ps -a --format="{{ .Names }}"'
         if self.dryrun:
             logger.info("DRY-RUN: %s", docker_cmd)
             return []
